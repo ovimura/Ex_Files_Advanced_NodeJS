8eddfd1930e3a89fd0898211c6164232df2eb103
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-15 12:57:27 -0700

    adding exercices for ch01 section 04

--- Exercise Files/Ch01/01_04/start/index.js
+++ Exercise Files/Ch01/01_04/start/index.js
@@ -1,18 +1,36 @@
-var delay = (seconds, callback) => {
-    if (seconds > 3) {
-        callback(new Error(`${seconds} seconds it too long!`));
-    } else {
-        setTimeout(() =>
-            callback(null, `the ${seconds} second delay is over.`),
-            seconds
-        );
-    }
-}
+var fs = require('fs');
+var { promisify } = require('util');
 
-delay(2, (error, message) => {
-    if (error) {
-        console.log(error.message);
-    } else {
-        console.log(message);
-    }
-});
+// var delay = (seconds, callback) => {
+//     if (seconds > 3) {
+//         callback(new Error(`${seconds} seconds it too long!`));
+//     } else {
+//         setTimeout(() =>
+//             callback(null, `the ${seconds} second delay is over.`),
+//             seconds
+//         );
+//     }
+// }
+
+// delay(4, (error, message) => {
+//     if (error) {
+//         console.log(error.message);
+//     } else {
+//         console.log(message);
+//     }
+// });
+
+
+// var promiseDelay = promisify(delay);
+
+// promiseDelay(5)
+// .then(console.log)
+// .catch((error) => console.log(`error: ${error.message}`));
+
+var writeFile = promisify(fs.writeFile);
+
+
+writeFile('sample.txt', 'This is a sample')
+.then(() => console.log('file successfully created'))
+.catch(() => console.log('error creating file'));
+
