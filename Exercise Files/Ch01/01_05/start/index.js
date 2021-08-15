3ec8c19e3356c6fba587aae1bddc6c021c6d0b3f
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-15 13:11:18 -0700

    adding exercices for ch01, section 05

--- Exercise Files/Ch01/01_05/start/index.js
+++ Exercise Files/Ch01/01_05/start/index.js
@@ -1,33 +1,59 @@
 var fs = require('fs');
+var { promisify } = require('util');
+var writeFile = promisify(fs.writeFile);
+var unlink = promisify(fs.unlink);
 var beep = () => process.stdout.write("\x07");
+var delay = (seconds) => new Promise((resolves) => {
+    setTimeout(resolves, seconds*1000);
+});
 
-const doStuffSequentially = () => {
-    console.log('starting');
-    setTimeout(() => {
-        console.log('waiting');
-        setTimeout(() => {
-            console.log('waiting some more');
-            fs.writeFile('file.txt', 'Sample File...', error => {
-                if (error) {
-                    console.error(error);
-                } else {
-                    beep();
-                    console.log('file.txt created')
-                    setTimeout(() => {
-                        beep();
-                        fs.unlink('file.txt', error => {
-                            if (error) {
-                                console.error(error);
-                            } else {
-                                console.log('file.txt removed');
-                                console.log('sequential execution complete');
-                            }
-                        })
-                    }, 3000)
-                }
-            });
-        }, 2000)
-    }, 1000)
-}
+const doStuffSequentially = () => Promise.resolve()
+.then(() => console.log('starting'))
+.then(() => delay(1))
+.then(() => 'waiting')
+.then(console.log)
+.then(() => delay(2))
+.then(() => writeFile('file.txt', 'Sample File...'))
+.then(beep)
+.then(() => 'file.txt created')
+.then(console.log)
+.then(() => delay(3))
+.then(() => unlink('file.txt'))
+.then(beep)
+.then(() => 'file.txt removed')
+.then(console.log)
+.catch(console.error);
+
+
+
+
+// const doStuffSequentially = () => {
+//     console.log('starting');
+//     setTimeout(() => {
+//         console.log('waiting');
+//         setTimeout(() => {
+//             console.log('waiting some more');
+//             fs.writeFile('file.txt', 'Sample File...', error => {
+//                 if (error) {
+//                     console.error(error);
+//                 } else {
+//                     beep();
+//                     console.log('file.txt created')
+//                     setTimeout(() => {
+//                         beep();
+//                         fs.unlink('file.txt', error => {
+//                             if (error) {
+//                                 console.error(error);
+//                             } else {
+//                                 console.log('file.txt removed');
+//                                 console.log('sequential execution complete');
+//                             }
+//                         })
+//                     }, 3000)
+//                 }
+//             });
+//         }, 2000)
+//     }, 1000)
+// }
 
 doStuffSequentially();
