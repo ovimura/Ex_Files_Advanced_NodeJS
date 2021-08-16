b1c3b06d4351b8b4d75a43aa757d21425656abcf
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-16 00:07:54 -0700

    adding exercices for ch01, section 08

--- Exercise Files/Ch01/01_08/start/index.js
+++ Exercise Files/Ch01/01_08/start/index.js
@@ -14,3 +14,31 @@ var tasks = [
   delay(3),
   delay(5)
 ];
+
+class PromiseQueue {
+  constructor(promises=[], concurrentCount=1) {
+    this.concurrentCount = concurrentCount;
+    this.total = promisses.length;
+    this.todo = promises;
+    this.running = [];
+    this.complete = [];
+  }
+
+  get runAnother() {
+    return (this.running.length < this.concurrentCount) && this.todo.length;
+  }
+
+  run() {
+    while(this.runAnother) {
+      var promise = this.todo.shift();
+      promise.then(() => {
+        this.complete.push(this.running.shift());
+        this.run();
+      })
+      this.running.push(promise);
+    }
+  }
+}
+
+var delayQueue = new PromiseQueue(tasks, 2);
+delayQueue.run();
