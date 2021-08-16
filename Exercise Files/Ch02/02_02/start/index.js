b1c108634b1296ff84fa35c0f60a37973a2c5b05
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-16 12:20:41 -0700

    adding exercices for ch02, section 02

--- Exercise Files/Ch02/02_02/start/index.js
+++ Exercise Files/Ch02/02_02/start/index.js
@@ -1,3 +1,5 @@
+const { Readable } = require('stream');
+
 const peaks = [
     "Tallac",
     "Ralston",
@@ -7,3 +9,48 @@ const peaks = [
     "Rose",
     "Freel Peak"
 ];
+
+class StreamFromArray extends Readable {
+
+    // constructor(array) {
+    //     super({ encoding: 'utf-8' });
+    //     this.array = array;
+    //     this.index = 0;
+    // }
+
+    // _read() {
+    //     if(this.index <= this.array.length) {
+    //     const chunk = this.array[this.index];
+    //     this.push(chunk);
+    //     this.index += 1;
+    //     } else {
+    //         this.push(null);
+    //     }
+    // }
+
+
+    constructor(array) {
+        super({ objectMode: true });
+        this.array = array;
+        this.index = 0;
+    }
+
+    _read() {
+        if(this.index <= this.array.length) {
+            const chunk = {
+                data: this.array[this.index],
+                index: this.index
+            }
+            this.push(chunk);
+            this.index += 1;
+        } else {
+            this.push(null);
+        }
+    }
+}
+
+const peakStream = new StreamFromArray(peaks);
+
+peakStream.on('data', (chunk) => console.log(chunk));
+
+peakStream.on('end', () => console.log('done!'));
