f625eda219234b8cbcc8b89d54e5457cb0047a26
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-16 00:00:09 -0700

    adding exercices for ch01, section 07

--- Exercise Files/Ch01/01_07/start/index.js
+++ Exercise Files/Ch01/01_07/start/index.js
@@ -7,3 +7,35 @@ var beep = () => process.stdout.write("\x07");
 var delay = (seconds) => new Promise((resolves) => {
     setTimeout(resolves, seconds*1000);
 })
+
+// Promise.all([
+//     writeFile('readme.md','Hello World'),
+//     writeFile('readme.txt', 'Hello World'),
+//     writeFile('readme.json', '{ "hello": "world" }')
+// ]).then(() => readdir(__dirname))
+// .then(console.log);
+
+// Promise.all([
+//     unlink('readme.md'),
+//     unlink('readme.txt'),
+//     delay(3),
+//     unlink('readme.json')
+// ]).then(() => readdir(__dirname))
+// .then(console.log);
+
+// Promise.all([
+//     delay(5),
+//     delay(2),
+//     delay(3),
+//     delay(5)
+// ]).then(() => readdir(__dirname))
+// .then(console.log);
+
+Promise.race([
+    delay(5),
+    delay(2),
+    delay(3),
+    delay(5)
+]).then(() => readdir(__dirname))
+.then(console.log);
+
