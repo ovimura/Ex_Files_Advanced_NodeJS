d0fa1d0cf33a39c005fb622c2a93f7fbb07f0e1a
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-17 08:36:52 -0700

    adding exercices for ch02, lesson 07

--- Exercise Files/Ch02/02_07/start/index.js
+++ Exercise Files/Ch02/02_07/start/index.js
@@ -1,6 +1,37 @@
+const { Duplex, PassThrough } = require('stream');
 const { createReadStream, createWriteStream } = require('fs');
 
 const readStream = createReadStream('../../powder-day.mp4')
 const writeStream = createWriteStream('./copy.mp4');
 
-readStream.pipe(writeStream);
+class Throttle extends Duplex {
+    constructor(ms) {
+        super();
+        this.delay = ms;
+    }
+
+    _write(chunk, encoding, callback) {
+        this.push(chunk);
+        setTimeout(callback, this.delay);
+    }
+
+    _read() {}
+
+    _final() {
+        this.PassThrough(null);
+    }
+}
+
+const report = new PassThrough();
+const throttle = new Throttle(100);
+
+var total = 0;
+report.on('data', (chunk) => {
+    total += chunk.length;
+    console.log('bytes: ', total);
+});
+
+readStream
+.pipe(throttle)
+.pipe(report)
+.pipe(writeStream);
