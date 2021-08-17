d3aad7160b3ff597a07af3160900ebb023fa733c
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-16 18:30:53 -0700

    adding exercices to chapter 02 lesson 06

--- Exercise Files/Ch02/02_06/start/index.js
+++ Exercise Files/Ch02/02_06/start/index.js
@@ -1,31 +1,11 @@
 const { createReadStream, createWriteStream } = require('fs');
 
-const readStream = createReadStream('../../powder-day.mp4');
-const writeStream = createWriteStream('./copy.mp4', {
-   //highWaterMark: 1628920128
-});
+//const readStream = createReadStream('../../powder-day.mp4');
+//const writeStream = createWriteStream('./copy.mp4');
+const writeStream = createWriteStream('./file.txt');
 
-readStream.on('data', (chunk) => {
-    const result = writeStream.write(chunk);
-    if (!result) {
-       console.log('backpressure')
-       readStream.pause();
-    }
-});
+//readStream.pipe(writeStream).on('error', console.error);
 
-readStream.on('error', (error) => {
-    console.log('an error occurred', error.message);
-});
+process.stdin.pipe(writeStream);
 
-readStream.on('end', () => {
-    writeStream.end();
-});
 
-writeStream.on('drain', () => {
-    console.log('drained');
-    readStream.resume();
-})
-
-writeStream.on('close', () => {
-    process.stdout.write('file copied\n');
-})
