5c09e16d378ef2715f3f8281696628e506305b2f
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-17 17:34:24 -0700

    adding exercices for ch03 lesson 04

--- Exercise Files/Ch03/03_04/start/index.js
+++ Exercise Files/Ch03/03_04/start/index.js
@@ -7,6 +7,7 @@ const {
 const { promisify } = require('util');
 const fileName = '../../powder-day.mp4';
 const fileInfo = promisify(stat);
+const multiparty = require('multiparty');
 
 const respondWithVideo = async (req, res) => {
   const { size } = await fileInfo(fileName);
@@ -33,9 +34,15 @@ const respondWithVideo = async (req, res) => {
 
 createServer((req, res) => {
   if (req.method === 'POST') {
-    req.pipe(res);
-    req.pipe(process.stdout);
-    req.pipe(createWriteStream('./upload.file'));
+    let form = new multiparty.Form();
+    form.on('part', (part) => {
+      part.pipe(createWriteStream(`./${part.filename}`))
+      .on('close', () => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`<h1>File uploaded: ${part.filename}</h1>`);
+      })
+    });
+    form.parse(req);
   } else if (req.url === '/video') {
     respondWithVideo(req, res);
   } else {
